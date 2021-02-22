2f210fadbf448b93c4fc63c22654e24d21ae1f11
Jeff Parker <jeffp42ker@gmail.com>
2021-02-22 15:53:30 -0500

    Add -M additional clj exec-opt when -m main-opt are called to avoid warnings.

--- new-project.sh
+++ new-project.sh
@@ -1,3 +1,3 @@
 sha="$(git ls-remote https://github.com/jeffp42ker/biff.git HEAD | awk '{ print $1 }')"
 deps="{:deps {github-jeffp42ker/biff {:git/url \"https://github.com/jeffp42ker/biff\" :sha \"$sha\"}}}"
-clj -Sdeps "$deps" -m biff.project
+clj -Sdeps "$deps" -M -m biff.project
